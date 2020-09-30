5ad880775e35c76f30615190c8fc1b9862af361d
omarvegaabreu <omarvega@hotmail.com>
2020-09-30 14:17:48 -0400

    wrapped component in card

--- src/Components/CommentDetail.js
+++ src/Components/CommentDetail.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { Comment } from "semantic-ui-react";
+import { Card, Comment } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 
 const CommentDetail = (props) => {
   return (
-    <Comment>
-      <Comment.Avatar src={props.image} />
-      <Comment.Content>
-        <Comment.Author as="a">{props.author}</Comment.Author>
-        <Comment.Metadata>
-          <div>{props.time}</div>
-        </Comment.Metadata>
-        <Comment.Text>{props.comment}</Comment.Text>
-        <Comment.Actions>
-          {/* <Comment.Action>Reply</Comment.Action> */}
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
+    <Card>
+      <Comment>
+        <Comment.Avatar src={props.image} />
+        <Comment.Content>
+          <Comment.Author as="a">{props.author}</Comment.Author>
+          <Comment.Metadata>
+            <div>{props.time}</div>
+          </Comment.Metadata>
+          <Comment.Text>{props.comment}</Comment.Text>
+          <Comment.Actions>
+            {/* <Comment.Action>Reply</Comment.Action> */}
+          </Comment.Actions>
+        </Comment.Content>
+      </Comment>
+    </Card>
   );
 };
 
